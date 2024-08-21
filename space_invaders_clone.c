c4773f5ebe84cb679edc02524459826dd8eb99d7
p1x3r <p1x3r@proton.me>
2024-08-21 18:36:28 -0500

    Player :b

--- space_invaders_clone.c
+++ space_invaders_clone.c
@@ -3,17 +3,27 @@
 int main(void) {
   // Initialize
   InitWindow(800, 450, "Space Invaders (clone)");
+  SetTargetFPS(60);
+
+  const float player_speed = 4.f;
+  const float player_size = 20.f;
+  Rectangle player = {GetScreenWidth() / 2.f - player_size,
+                      GetScreenHeight() - 100.f, player_size, player_size};
 
   // Game loop
   while (!WindowShouldClose()) {
     // Event handling
-    // TODO: Make the player movement
+    if (IsKeyDown(KEY_A)) {
+      player.x -= player_speed;
+    }
+    if (IsKeyDown(KEY_D)) {
+      player.x += player_speed;
+    }
 
     // Drawing
     BeginDrawing();
-    ClearBackground(RAYWHITE);
-    DrawText("Congrats! You created your first window!", 190, 200, 20,
-             LIGHTGRAY);
+    ClearBackground(BLACK);
+    DrawRectangleRec(player, GREEN);
     EndDrawing();
   }
 
