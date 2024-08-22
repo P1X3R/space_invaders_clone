01545e6509374f13624df4e23cac5f296ccd2604
p1x3r <p1x3r@proton.me>
2024-08-21 19:03:03 -0500

    Make the enemies >:b

--- space_invaders_clone.c
+++ space_invaders_clone.c
@@ -1,5 +1,7 @@
 #include "raylib.h"
 
+typedef enum { left = -1, right = 1 } directions;
+
 int main(void) {
   // Initialize
   InitWindow(800, 450, "Space Invaders (clone)");
@@ -9,6 +11,25 @@ int main(void) {
   const float player_size = 20.f;
   Rectangle player = {GetScreenWidth() / 2.f - player_size,
                       GetScreenHeight() - 100.f, player_size, player_size};
+
+  const unsigned int enemies_number = 48;
+  const unsigned int enemies_per_row = 12;
+  directions enemies_direction = right;
+  Rectangle enemies[enemies_number];
+
+  for (unsigned int i = 0; i < enemies_number; i++) {
+    if (i == 0) {
+      enemies[i] = (Rectangle){10.f, 10.f, player_size, player_size};
+    } else {
+      enemies[i] = (Rectangle){enemies[i - 1].x + player_size + 10,
+                               enemies[i - 1].y, player_size, player_size};
+    }
+
+    if (i % enemies_per_row == 0 && i != 0) {
+      enemies[i].x = 10.f;
+      enemies[i].y = enemies[i - 1].y + player_size + 10.f;
+    }
+  }
 
   // Game loop
   while (!WindowShouldClose()) {
@@ -24,6 +45,19 @@ int main(void) {
     BeginDrawing();
     ClearBackground(BLACK);
     DrawRectangleRec(player, GREEN);
+
+    // Enemies operations
+    for (unsigned int i = 0; i < enemies_number; i++) {
+      enemies[i].x += enemies_direction;
+
+      if (enemies[i].x > GetScreenWidth() - player_size) {
+        enemies_direction = left;
+      } else if (enemies[i].x < 0) {
+        enemies_direction = right;
+      }
+
+      DrawRectangleRec(enemies[i], WHITE);
+    }
     EndDrawing();
   }
 
