a94a6d46bc32577e7405ead643ff90e9353754de
p1x3r <p1x3r@proton.me>
2024-08-22 16:49:38 -0500

    Handle the enemies with another function

--- space_invaders_clone.c
+++ space_invaders_clone.c
@@ -2,32 +2,47 @@
 
 typedef enum { left = -1, right = 1 } directions;
 
+void enemiesHandling(Rectangle *enemies, const unsigned int enemiesNumber,
+                     const float playerSize, directions enemiesDirection) {
+  for (unsigned int i = 0; i < enemiesNumber; i++) {
+    enemies[i].x += enemiesDirection;
+
+    if (enemies[i].x > GetScreenWidth() - playerSize) {
+      enemiesDirection = left;
+    } else if (enemies[i].x < 0) {
+      enemiesDirection = right;
+    }
+
+    DrawRectangleRec(enemies[i], WHITE);
+  }
+}
+
 int main(void) {
   // Initialize
   InitWindow(800, 450, "Space Invaders (clone)");
   SetTargetFPS(60);
 
-  const float player_speed = 4.f;
-  const float player_size = 20.f;
-  Rectangle player = {GetScreenWidth() / 2.f - player_size,
-                      GetScreenHeight() - 100.f, player_size, player_size};
+  const float playerSpeed = 4.f;
+  const float playerSize = 20.f;
+  Rectangle player = {GetScreenWidth() / 2.f - playerSize,
+                      GetScreenHeight() - 100.f, playerSize, playerSize};
 
-  const unsigned int enemies_number = 48;
-  const unsigned int enemies_per_row = 12;
-  directions enemies_direction = right;
-  Rectangle enemies[enemies_number];
+  const unsigned int enemiesNumber = 48;
+  const unsigned int enemiesPerRow = 12;
+  directions enemiesDirection = right;
+  Rectangle enemies[enemiesNumber];
 
-  for (unsigned int i = 0; i < enemies_number; i++) {
+  for (unsigned int i = 0; i < enemiesNumber; i++) {
     if (i == 0) {
-      enemies[i] = (Rectangle){10.f, 10.f, player_size, player_size};
+      enemies[i] = (Rectangle){10.f, 10.f, playerSize, playerSize};
     } else {
-      enemies[i] = (Rectangle){enemies[i - 1].x + player_size + 10,
-                               enemies[i - 1].y, player_size, player_size};
+      enemies[i] = (Rectangle){enemies[i - 1].x + playerSize + 10,
+                               enemies[i - 1].y, playerSize, playerSize};
     }
 
-    if (i % enemies_per_row == 0 && i != 0) {
+    if (i % enemiesPerRow == 0 && i != 0) {
       enemies[i].x = 10.f;
-      enemies[i].y = enemies[i - 1].y + player_size + 10.f;
+      enemies[i].y = enemies[i - 1].y + playerSize + 10.f;
     }
   }
 
@@ -35,36 +50,24 @@ int main(void) {
   while (!WindowShouldClose()) {
     // Event handling
     if (IsKeyDown(KEY_A)) {
-      player.x -= player_speed;
+      player.x -= playerSpeed;
     }
     if (IsKeyDown(KEY_D)) {
-      player.x += player_speed;
+      player.x += playerSpeed;
     }
 
     // Player dege collision
     if (player.x < 0) {
       player.x = 0;
-    } else if (player.x > GetScreenWidth() - player_size) {
-      player.x = GetScreenWidth() - player_size;
+    } else if (player.x > GetScreenWidth() - playerSize) {
+      player.x = GetScreenWidth() - playerSize;
     }
 
     // Drawing
     BeginDrawing();
     ClearBackground(BLACK);
     DrawRectangleRec(player, GREEN);
-
-    // Enemies operations
-    for (unsigned int i = 0; i < enemies_number; i++) {
-      enemies[i].x += enemies_direction;
-
-      if (enemies[i].x > GetScreenWidth() - player_size) {
-        enemies_direction = left;
-      } else if (enemies[i].x < 0) {
-        enemies_direction = right;
-      }
-
-      DrawRectangleRec(enemies[i], WHITE);
-    }
+    enemiesHandling(enemies, enemiesNumber, playerSize, enemiesDirection);
     EndDrawing();
   }
 
