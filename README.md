0b5e59adecb11d96142ca2b3e4e76f0bf24c1fdc
p1x3r <p1x3r@proton.me>
2024-08-23 15:01:38 -0500

    Make the player shoot

---
+++ README.md
@@ -0,0 +1,2 @@
+# Space Invaders clone
+Just another Space Invader clone made in C for learning.
--- space_invaders_clone.c
+++ space_invaders_clone.c
@@ -3,18 +3,60 @@
 typedef enum { left = -1, right = 1 } directions;
 
 typedef struct {
-  float size;
-  Rectangle body;
-  float speed;
+  float size, speed;
+  Rectangle body, bullets[5];
+  unsigned int bulletNumber;
 } player;
 
 typedef struct {
   Rectangle array[48];
-  unsigned int enemiesPerRow;
-  unsigned int number;
+  unsigned int enemiesPerRow, number;
   bool moveDown;
   directions direction;
 } enemies;
+
+void initializingLoop(enemies *enemies, player *player, float bulletsDefaultY,
+                      float bulletsSize) {
+  for (unsigned int i = 0; i < enemies->number; i++) {
+    if (i == 0) {
+      enemies->array[i] = (Rectangle){10.f, 10.f, player->size, player->size};
+    } else {
+      enemies->array[i] =
+          (Rectangle){enemies->array[i - 1].x + player->size + 10,
+                      enemies->array[i - 1].y, player->size, player->size};
+    }
+
+    if (i % enemies->enemiesPerRow == 0 && i != 0) {
+      enemies->array[i].x = 10.f;
+      enemies->array[i].y = enemies->array[i - 1].y + player->size + 10.f;
+    }
+
+    if (i < player->bulletNumber) {
+      player->bullets[i] =
+          (Rectangle){player->body.x, bulletsDefaultY, 2.f, bulletsSize};
+    }
+  }
+}
+
+void shoot(player *player) {
+  for (unsigned int i = 0; i < player->bulletNumber; i++) {
+    if (player->bullets[i].y < -9.f) {
+      player->bullets[i].x = player->body.x + player->body.width / 2;
+      player->bullets[i].y = player->body.y + player->body.height / 2;
+      break;
+    }
+  }
+}
+
+void playerBulletsHandling(player *player, unsigned int i) {
+  if (i > player->bulletNumber) {
+    return;
+  }
+
+  player->bullets[i].y -= 10.f;
+
+  DrawRectangleRec(player->bullets[i], YELLOW);
+}
 
 // TODO: Make the enemies shoot
 void enemiesHandling(enemies *enemies, const float size, unsigned int i) {
@@ -41,37 +83,32 @@ int main(void) {
   InitWindow(800, 450, "Space Invaders (clone)");
   SetTargetFPS(60);
 
-  player player = {20.f,
-                   {GetScreenWidth() / 2.f - player.size,
-                    GetScreenHeight() - 100.f, player.size, player.size},
-                   4.f};
+  const float bulletsSize = 10.f;
+  const float bulletsDefaultY = bulletsSize * -1;
+  player player = {
+      20.f, // Size
+      4.f,  // Speed
+      {GetScreenWidth() / 2.f - player.size, GetScreenHeight() - 100.f,
+       player.size, player.size}, // Body
+      {{0, 0, 0, 0}},             // Bullets
+      5                           // Bullet number
+  };
 
   enemies enemies = {{{0}}, 12, 48, false, right};
 
-  for (unsigned int i = 0; i < enemies.number; i++) {
-    if (i == 0) {
-      enemies.array[i] = (Rectangle){10.f, 10.f, player.size, player.size};
-    } else {
-      enemies.array[i] =
-          (Rectangle){enemies.array[i - 1].x + player.size + 10,
-                      enemies.array[i - 1].y, player.size, player.size};
-    }
-
-    if (i % enemies.enemiesPerRow == 0 && i != 0) {
-      enemies.array[i].x = 10.f;
-      enemies.array[i].y = enemies.array[i - 1].y + player.size + 10.f;
-    }
-  }
+  initializingLoop(&enemies, &player, bulletsDefaultY, bulletsSize);
 
   // Game loop
   while (!WindowShouldClose()) {
     // Event handling
-    // TODO: Make the player shoot
     if (IsKeyDown(KEY_A)) {
       player.body.x -= player.speed;
     }
     if (IsKeyDown(KEY_D)) {
       player.body.x += player.speed;
+    }
+    if (IsKeyPressed(KEY_SPACE)) {
+      shoot(&player);
     }
 
     // Player dege collision
@@ -84,10 +121,13 @@ int main(void) {
     // Drawing
     BeginDrawing();
     ClearBackground(BLACK);
-    DrawRectangleRec(player.body, GREEN);
+
     for (unsigned int i = 0; i < enemies.number; i++) {
+      playerBulletsHandling(&player, i);
       enemiesHandling(&enemies, player.size, i);
     }
+
+    DrawRectangleRec(player.body, GREEN);
     EndDrawing();
   }
 
